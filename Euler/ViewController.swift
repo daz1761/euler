36288da071bff2acab6c6ae618a3d5b7e2dcc132
Darryl Griffiths <dazgti@btinternet.com>
2018-01-30 15:37:41 +0000

    Completed:  Even Fibonacci numbers

--- Euler/ViewController.swift
+++ Euler/ViewController.swift
@@ -11,15 +11,29 @@ import UIKit
 class ViewController: UIViewController {
     
     private var myModel = model()
+    private var fib_array = [Int]()
 
     @IBOutlet weak var answerLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let answers = myModel.getMultiples()
-        let sum = answers.reduce(0, +)
-        answerLabel.text = String(sum)
+        // multiples
+        let multiple_ans = myModel.getMultiples()
+        let multiple_sum = multiple_ans.reduce(0, +)  // sum array
+        answerLabel.text = String(multiple_sum)
+        
+        // fibonacci
+        let fib_ans = myModel.fibonacciSequence()
+        
+        for fib in fib_ans {
+            if fib % 2 == 0 {
+                self.fib_array.append(fib)
+                
+            }
+        }
+        let fib_sum = fib_array.reduce(0, +)
+        print(fib_sum)
     }
 
 }
--- Euler/model.swift
+++ Euler/model.swift
@@ -10,30 +10,52 @@ import Foundation
 
 class model {
     
-    var arr = [Int]()
+    var multiple_arr = [Int]()
     var multiples = [Int]()
     let three = 3
     let five = 5
-    var sum : Int?
+    let total = 4000000 // 4,000,000,00 (4 million)
+    var fib = [Int]()
     
     init() {
-        self.arr += 1...999 // populate the array using the "range operator" from 1 to 999
-        //print(arr)
+        self.multiple_arr += 1...999 // populate the array using the "range operator" from 1 to 999
     }
     
     func getMultiples() -> Array<Int> {
         
-        for num in arr {
+        for num in multiple_arr {
             
             if( (num % self.three == 0) || (num % self.five == 0) ) {
                 self.multiples.append(num)
             }
             
         }
-        //self.sum = self.answer.reduce(0, +)
         
-        //return sum
         return multiples
+    }
+    
+    func fibonacciSequence() -> Array<Int> {
+        
+        var num = 0
+        
+        for i in 0...30 {
+      
+            if(i==0) {
+                fib.append(1)
+                num = fib[i] + fib[i]
+                fib.append(num)
+            }
+            if(i>0) {
+                num = fib[i-1] + fib[i]
+                fib.append(num)
+            }
+        }
+        
+        
+        // 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+        // my answer : [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+        
+        return fib
     }
     
 }
